cc0dcd9ad94b44de457f47d0398f82f9fde5e25d
nunnunannaa <hhj9523@gmail.com>
2018-10-28 11:51:40 +0900

    Fourth Commit

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
-#include <stdio.h>
-int main() {
-
-  printf("Hello");
+public class Hello {
+  public static void main(String[] args){
+    System.out.println("Hello, world from GiT!");
+    
+  }
 }
+
